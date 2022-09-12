8e2aeb224a8e398e3d85494de45c2fa24a5372fe
Jason Brown <jason@jajabromba.local>
2022-09-12 22:02:19 +0200

    adds express get uri for each turtle file path.  returns the json-ld document as response to the http get.

--- src/index.ts
+++ src/index.ts
@@ -4,44 +4,111 @@ import morgan from 'morgan';
 import * as fs from 'fs';
 import * as stream from 'stream';
 import * as n3 from 'n3';
+import path from 'path';
+
+const ttl2jsonld = require('@frogcat/ttl2jsonld').parse;
 
 const app = Express();
 var store = new n3.Store();
+
+function getAllFiles(dirPath: string){
+    fs.readdirSync(dirPath).forEach(function(file) {
+    let filepath = path.join(dirPath , file);
+    let stat= fs.statSync(filepath);
+    if (stat.isDirectory()) {            
+      getAllFiles(filepath);
+    } else {
+          console.info(dirPath + '\n');
+          console.info(filepath+ '\n');                      
+    }    
+});  
+}
+
+function addallFilePaths(dirPath: string){
+    fs.readdirSync(dirPath).forEach(function(file) {
+    let filepath = path.join(dirPath , file);
+    let stat= fs.statSync(filepath);
+    if (stat.isDirectory()) {            
+        addallFilePaths(filepath);
+    } else {
+        const fileComponents: string[] = filepath.split('.');
+        console.info('Get URI: ' + fileComponents[0] + '\n');
+        app.get('/' + fileComponents[0], (req, res) => {
+            const turtle = fs.readFileSync(filepath, 'utf-8');
+            const jsonld = ttl2jsonld(turtle);
+            //console.log(jsonld);
+            //console.log(JSON.stringify(jsonld,null,2));
+            res.setHeader('Content-Type', 'application/json')
+            res.send(JSON.stringify(jsonld,null,2));
+        }); 
+    }    
+});  
+}
+
+process.chdir('schemas')
+addallFilePaths('.');
 
 app.use(Express.json());
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
 
-
 // logging
 app.use(morgan('combined'));
 
-app.get('/', (req, res) => {
 
+
+
+
+app.get('/', (req, res) => {
     const parser = new n3.Parser();
     const turtle = fs.readFileSync('schemas/portfolio.ttl', 'utf-8');
 
     //    rdfStream = fs.createReadStream('schemas/portfolio.ttl');
     //    rdfStream.pipe(parser).on()
+    /*
     parser.parse(turtle,
         (error, triple, prefixes) => {
             if (triple) {
                 store.addQuad(triple);
-                // console.log(triple);
+                console.log(triple);
                 console.log(triple.predicate.id.toString());
-            }
-            else if (prefixes) {
-                console.log('PREFIX ' + prefixes);
             }
             else {
                 console.log("# That's all, folks!", prefixes);
             }
         });
+    */
 
-    store.getQuads(null, 'http://www.w3.org/2000/01/rdf-schema#Class', null, null).forEach()
+    const quads: n3.Quad[] =parser.parse(turtle);
+
+    quads.forEach((quad) => {
+        store.addQuad(quad);
+        console.log(quad);
+    });
+
+    console.log("Retreiveing all triples");
+    
+    store.getQuads(null, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://www.w3.org/2000/01/rdf-schema#Class', null).forEach((quad) => {
+        console.log(quad);
+        store.getQuads(quad.subject.id, 'http://www.w3.org/2000/01/rdf-schema#Property', null, null).forEach((quad) => {
+            console.log(quad);
+            n3.Util.prefix
+        });
+    });
+
     res.send('Hello World!')
 });
 
+app.get('/jsonld', (req, res) => {
+    const baseDir = 'schemas';
+    getAllFiles(baseDir);
+
+    const turtle = fs.readFileSync('schemas/portfolio.ttl', 'utf-8');
+    const jsonld = ttl2jsonld(turtle);
+    console.log(jsonld);
+    console.log(JSON.stringify(jsonld,null,2));
+    res.send(JSON.stringify(jsonld,null,2));
+});
 // start server
 app.listen(9000, () => console.info('api listening at http://localhost:9000'));
